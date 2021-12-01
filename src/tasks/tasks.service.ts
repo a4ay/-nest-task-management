228c27c6497ec7e64afbb0246816f0575dd0a843
a4ay <pradhanajay1999@gmail.com>
2021-12-01 19:42:17 +0530

    in progress

--- src/tasks/tasks.service.ts
+++ src/tasks/tasks.service.ts
@@ -1,8 +1,9 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
 import { v4 as uuid } from 'uuid';
 import { CreateTaskDto } from './dto/create-task.dto';
 import { Task, TaskStatus } from './task.model';
 import { GetTasksFilterDto } from './dto/filter-task.dto';
+import { NotFoundError } from 'rxjs';
 
 @Injectable()
 export class TasksService {
@@ -30,7 +31,11 @@ export class TasksService {
   }
 
   getTaskById(id: string): Task {
-    return this.tasks.find((task) => task.id === id);
+    const task = this.tasks.find((task) => task.id === id);
+    if (!task) {
+      throw new NotFoundException(`Task with id ${id} not found!`);
+    }
+    return task;
   }
 
   postTask(createTaskDto: CreateTaskDto): Task {
@@ -49,9 +54,6 @@ export class TasksService {
 
   deleteTask(id: string): Task {
     const task = this.getTaskById(id);
-    if (!task) {
-      throw new Error('No such task exists!');
-    }
     this.tasks = this.tasks.filter((task) => task.id !== id);
     return task;
   }
